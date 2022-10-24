8612130129d0ed18b8d6b2cc07511c0f1f2849bf
Sherlock Holmes <sherlockholmesxp@gmail.com>
2022-10-24 23:30:16 +0800

    Edits

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-// index.js
 const Eris = require('eris');
 require('dotenv').config();
 const bot = new Eris(process.env.TOKEN);
 // Replace TOKEN with your bot account's token
 
-bot.on("ready", () => { // When the bot is ready
-    console.log("Ready!"); // Log "Ready!"
+bot.on('ready', () => {
+	console.log('Ready!');
 });
 
-bot.on("error", (err) => {
-  console.error(err); // or your preferred logger
+bot.on('error', (err) => {
+	console.error(err);
 });
 
-bot.on("messageCreate", (message) => { // When a message is created
-    if(message.content === "!ping") { // If the message content is "!ping"
-        message.channel.createMessage("Pong!");
-        // Send a message in the same channel with "Pong!"
-    } else if(message.content === "!pong") { // Otherwise, if the message is "!pong"
-        message.channel.createMessage("Ping!");
-        // Respond with "Ping!"
-    }
+bot.on('messageCreate', (message) => {
+	if(message.content === '!ping') {
+		message.channel.createMessage('Pong!');
+	}
+	else if(message.content === '!pong') {
+		message.channel.createMessage('Ping!');
+	}
 });
 
-bot.connect(); // Get the bot to connect to Discord
+bot.connect();
