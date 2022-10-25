2663c54f7743f0acc13b40052ecadd6dad275e85
SherlockHolmes <>
2022-10-25 03:50:30 +0000

    Added method to keep bot online

--- index.js
+++ index.js
@@ -21,3 +21,13 @@ bot.on('messageCreate', (message) => {
 });
 
 bot.connect();
+
+const express = require('express')
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+  res.send('The bot is online')
+})
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
