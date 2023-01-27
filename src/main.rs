6d51b815829789edeb6ea47b3a777acac448e47a
Sherlock Holmes <sherlockholmesxp@gmail.com>
2023-01-27 14:07:13 +0800

    idk what to call this commit

---
+++ src/main.rs
@@ -0,0 +1,58 @@
+use std::env;
+
+use serenity::async_trait;
+use serenity::prelude::*;
+use serenity::model::channel::Message;
+use serenity::framework::standard::macros::{command, group};
+use serenity::framework::standard::{StandardFramework, CommandResult};
+use serde::{Serialize, Deserialize};
+
+#[group]
+#[commands(ping, blog)]
+struct General;
+
+struct Handler;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct GithubRepo {
+    x: i32,
+    y: i32,
+}
+
+#[async_trait]
+impl EventHandler for Handler {}
+
+#[tokio::main]
+async fn main() {
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("!")) // set the bot's prefix to "!"
+        .group(&GENERAL_GROUP);
+
+    // Login with a bot token from the environment
+    let token = env::var("DISCORD_TOKEN").expect("token");
+    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+    let mut client = Client::builder(token, intents)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pong!").await?;
+
+    Ok(())
+}
+
+#[command]
+async fn blog(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Here is the link to my blog: https://tuxpress.tk").await?;
+
+    Ok(())
+}
